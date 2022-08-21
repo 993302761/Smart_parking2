f99f19ae62bf1d31cc2c0c6f61365a3457741158
雷宇琪 <993302761@qq.com>
2022-08-21 11:45:53 +0800

    update

---
+++ smart_parking/Smart_parking_server/module_User/src/main/java/com/example/user/config/ConverterConfig.java
@@ -0,0 +1,15 @@
+package com.example.user.config;
+
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+
+public class ConverterConfig {
+    //  RabbitMQ会对发送对象做序列化，spring对消息对象的处理是由MessageConverter处理,
+    //  默认实现的是SimpleMessageConverter，基于JDK的ObjectOutPutStream完成序列化
+    //  若要修改，只需要定义一个MessageConverter类型的Bean就好
+    @Bean
+    public MessageConverter jsonMessageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+}
