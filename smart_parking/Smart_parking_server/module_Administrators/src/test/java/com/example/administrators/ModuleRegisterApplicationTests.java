05ae48f77e73407554ed159e5dfbdca25cf974a4
雷宇琪 <993302761@qq.com>
2022-08-29 22:08:10 +0800

    加入日志系统，但还需完善，暂时仅用于超级管理员部分

--- smart_parking/Smart_parking_server/module_Administrators/src/test/java/com/example/administrators/ModuleRegisterApplicationTests.java
+++ smart_parking/Smart_parking_server/module_Administrators/src/test/java/com/example/administrators/ModuleRegisterApplicationTests.java
@@ -1,14 +1,15 @@
 package com.example.administrators;
 
-import com.example.administrators.serviceImpl.AdministratorsServiceImpl;
+
 import org.junit.jupiter.api.Test;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ModuleRegisterApplicationTests {
-    private static final Logger LOGGER= LoggerFactory.getLogger(AdministratorsServiceImpl.class);
+    private static final Logger LOGGER= LoggerFactory.getLogger(ModuleRegisterApplicationTests.class);
 
     @Test
     void contextLoads() {
